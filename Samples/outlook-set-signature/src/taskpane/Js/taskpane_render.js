0f381840eea968f8951f4cd47f5bd8426fba0ad5
Sam Ramon <15154970+samantharamon@users.noreply.github.com>
2025-01-29 19:01:35 -0800

    Add debug statement

--- Samples/outlook-set-signature/src/taskpane/Js/taskpane_render.js
+++ Samples/outlook-set-signature/src/taskpane/Js/taskpane_render.js
@@ -125,6 +125,8 @@ function create_user_info() {
     // localStorage.setItem('user_info', JSON.stringify(user_info));
     Office.context.roamingSettings.set('user_info_form', JSON.stringify(user_info));
     Office.context.roamingSettings.saveAsync(result => { console.log(result); });
+    const testRoaming = Office.context.roamingSettings.get('user_info_form');
+    console.log(`Debug create_user_info: ${testRoaming}`);
     navigate_to_taskpane_assignsignature();
   }
 }
