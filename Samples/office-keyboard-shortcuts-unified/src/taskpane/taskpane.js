6851a433bafc962b5bb89e398e9d8fc20acbd840
Sam Ramon <15154970+samantharamon@users.noreply.github.com>
2026-03-23 12:40:03 -0700

    Fix

--- Samples/office-keyboard-shortcuts-unified/src/taskpane/taskpane.js
+++ Samples/office-keyboard-shortcuts-unified/src/taskpane/taskpane.js
@@ -1,4 +1,7 @@
 Office.onReady(() => {
+  document.getElementById("sideload-msg").style.display = "none";
+  document.getElementById("app-body").style.display = "flex";
+
   Office.actions.associate("ShowTaskpane", () => {
     return Office.addin
       .showAsTaskpane()
